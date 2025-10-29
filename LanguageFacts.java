a6148da795d8a99be1390fd11c3ea8d292b01907
Daniel Myers <64658614+Danluk1001@users.noreply.github.com>
2025-10-29 17:54:55 -0400

    Add files via upload

---
+++ LanguageFacts.java
@@ -0,0 +1,11 @@
+public class LanguageFacts{
+    public static void main(String[] args) {
+        System.out.println("Java is a class-based language");
+
+        System.out.println("Java classes have a 'main' method");
+
+        System.out.println("Java Statements end with a semicolon");
+
+        System.out.println("Programming is fun at times, but truly tedious");
+    }
+}
